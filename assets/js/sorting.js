aeced0ef0da11183a83209b5fe416134518e1378
madirusso <134986186+madirusso@users.noreply.github.com>
2023-11-13 22:56:31 +0000

    publications category sorting and button styling

--- assets/js/sorting.js
+++ assets/js/sorting.js
@@ -24,6 +24,26 @@ document.querySelector('#sort-reverse').addEventListener('click', () => {
     const lists = Array.from(container.querySelectorAll('.sortable'));
 
     if (iscategorized) {
-        lists.sort
+        lists.sort((a, b, c, d, e) => {
+            const biomech = a.classList.contains('biomech') ? 'biomech' : '';
+            const neurosci = b.classList.contains('neurosci') ? 'neurosci' : '';
+            const robot = c.classList.contains('robot') ? 'robot' : '';
+            const fluid = d.classList.contains('fluid') ? 'fluid' : '';
+            const morph = e.classList.contains('morph') ? 'morph' : '';
+                  
+        });
     }
-}); */
+
+    container.innerHTML = '';
+    for (const list of lists) {
+      container.appendChild(list);
+    }
+  
+    updateHeading();
+}); 
+
+function updateHeading() {
+    const heading = document.querySelector('#category-heading');
+    heading.textContent = isSortedByCategory ? 'Sorted by Category' : 'Original Category';
+} */
+
