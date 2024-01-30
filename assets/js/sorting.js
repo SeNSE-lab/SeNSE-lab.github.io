0e8efc44252211393a3f81f42d88303fb258f9e9
madirusso <134986186+madirusso@users.noreply.github.com>
2024-01-29 21:33:10 -0600

    mobile formatting (#29)
    * added red bar to all pages
    
    * updated alumni and layout
    
    * removed buttons and formatted images
    
    * added new pubs and links
    
    * footer colors
    
    * footer colors
    
    * footer colors
    
    * updated news posts
    
    * footer colors 2
    
    * home page content and FAQs
    
    * formatting fixes
    
    * fix media button
    
    * add chronological sorting for publications
    
    * publications category sorting and button styling
    
    * fix jump to patents bug
    
    * pub links formatting
    
    * update research
    
    * pub link styling
    
    * navbar updates
    
    * misc formatting
    
    * fix navbar on software page
    
    * edit alumni description
    
    * mobile formatting
    
    * fix people page
    
    * news and media page
    
    * news formatting
    
    * navbar update
    
    * navbar fix
    
    * news and media page launch
    
    * add news and media to nav bar
    
    * add robotics to nav bar
    
    * new pubs
    
    * center robotics content
    
    * create new publications page (not public)
    
    * small styling changes
    
    * pubs formatting
    
    * mobile formatting
    
    * mobile formatting

--- assets/js/sorting.js
+++ assets/js/sorting.js
@@ -1,23 +1,34 @@
 //for old publications page:
-document.querySelector('#sort-reverse').addEventListener('click', () => {
+ document.querySelector('#sort-reverse').addEventListener('click', () => {
     //isreversed = !isreversed;
     const container = document.querySelector('#list-of-publications');
     const lists = Array.from(container.querySelectorAll('.sortable'));
-    
-    //if (isreversed) {
-        lists.reverse();
-    //}
+ 
+    lists.reverse();
+
 
     container.innerHTML = '';
 
     for (const list of lists) {
         container.appendChild(list);
     }
+}); 
+
+document.querySelector('#sort-reverse-table').addEventListener('click', () => {
+    const tablecontainer = document.querySelector('#table-of-publications');
+    const tablelists = Array.from(tablecontainer.querySelectorAll('.sortable-rows'));
+    
+    tablelists.reverse();
+    
+    tablecontainer.innerHTML = '';
+
+    for (const div of tablelists) {
+        tablecontainer.appendChild(div);
+    }
 });
 
-
 //for new publications page:
-document.querySelector('#sort-reverse-table').addEventListener('click', () => {
+/* document.querySelector('#sort-reverse-table').addEventListener('click', () => {
     
     const table = document.querySelector('#table-of-publications');
     const rows = Array.from(table.querySelectorAll('.sortable-rows'));
@@ -30,7 +41,7 @@ document.querySelector('#sort-reverse-table').addEventListener('click', () => {
     for (const row of rows) {
         tbody.appendChild(row); // Appending the sorted (or reversed) rows back to the table body
     }
-});
+}); */
 
 
 
